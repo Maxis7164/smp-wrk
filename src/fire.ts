a0c748bdd810cbdd4ebd0c80c886b6c73d939902
Maxis <maxdopp.md@gmail.com>
2024-04-20 17:32:22 +0200

    + vuefire

---
+++ src/fire.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdzNlSAVqMLq7JIWwonzJztS1eMROCJyY",
+  authDomain: "smp-sdl.firebaseapp.com",
+  projectId: "smp-sdl",
+  storageBucket: "smp-sdl.appspot.com",
+  messagingSenderId: "1054742679816",
+  appId: "1:1054742679816:web:388a0c329927f9b7515cbd",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import {
 } from "vue-router";
 import { createApp } from "vue";
 import "./style.scss";
+import { VueFire, VueFireAuth } from "vuefire";
+import { firebaseApp } from "./fire";
 
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
 import Settings from "./pages/Settings.vue";
 import Load from "./pages/Load.vue";
 
+const app = createApp(App);
+
+//#region VueRouter
 const routes: RouteRecordRaw[] = [
   { path: "/", component: Home },
   { path: "/settings", component: Settings },
@@ -22,8 +27,16 @@ const router = createRouter({
   routes,
 });
 
-const app = createApp(App);
-
 app.use(router);
+//#endregion
+
+//#region VueFire
+
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+});
+
+//#endregion
 
 app.mount("#app");
